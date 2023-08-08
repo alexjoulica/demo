786f86bb4acd8d9123af2aef3f35998b8869488d
alexjoulica <139750354+alexjoulica@users.noreply.github.com>
2023-08-08 15:36:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+/*function submitBtn1() {
+    document.getElementById(btn1)
+    
+}
+function submitBtn2() {
+    document.getElementById(btn2)
+}
+function submitBtn3() {
+    document.getElementById(btn3)
+}
+function submitBtn4() {
+    document.getElementById(btn4)
+}
+function submitBtn5() {
+    document.getElementById(btn5)
+}
+*/
